8def18a65d9336384f16a4d51cf44590d8c3ad2a
thinhofdakh <0306201587@caothang.edu.vn>
2024-02-22 15:53:55 +0700

    finish_test_edit_my_hotels

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -64,3 +64,50 @@ test('should display hotels', async ({page}) => {
   await expect(page.getByRole('link', {name: 'View Details'})).toBeVisible();
   await expect(page.getByRole('link', {name: 'Add hotel'})).toBeVisible();
 });
+
+test('should allow user to edit a hotel', async ({page}) => {
+  await page.goto(`${UI_URL}my-hotels`);
+  await page.getByRole('link', {name: 'View details'}).click();
+  await page.waitForSelector('[name="name"]', {state: 'attached'});
+  await expect(page.locator('[name=name]')).toHaveValue('Pink Three');
+  await page.locator('[name=name]').fill('Pink Three');
+  await page.locator('[name=city]').fill('Ho Chi Minh City');
+  await page.locator('[name=country]').fill('Vietnamese');
+  await page.locator('[name=description]').fill(`General Information:
+  - Hotel Name: Choose a catchy and memorable name that reflects the generator concept.
+  - Location: Briefly describe the neighborhood and its proximity to key attractions.
+  - Price Range: Mention the general price range to attract the right audience.
+  - Unique Selling Points: Highlight what makes your hotel stand out from other generator options.
+  
+  Generator Vibe:
+  - Community Focus: Describe the social atmosphere and opportunities for guests to connect.
+  - Events and Activities: Mention any workshops, performances, or social events hosted by the hotel.
+  - Design and Decor: Describe the overall aesthetic that reflects the generator lifestyle.
+  - Sustainability: If applicable, emphasize eco-friendly practices and initiatives.`);
+  await page.locator('[name=pricePerNight]').fill('200');
+  await page.selectOption('select[name=starRating]', '4');
+  await page.getByText('Budget').click();
+  await page.getByLabel('Free Wifi').check();
+  await page.getByLabel('Parking').check();
+  await page.locator('[name=adultCount]').fill('1');
+  await page.locator('[name=childCount]').fill('0');
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, 'files', '1.jpg'),
+    path.join(__dirname, 'files', '2.jpg'),
+    path.join(__dirname, 'files', '3.jpg'),
+    path.join(__dirname, 'files', '4.jpg'),
+    path.join(__dirname, 'files', '5.jpg')
+  ]);
+
+  await page.getByRole('button', {name: 'Save'}).click();
+  page.setDefaultTimeout(20000);
+  await expect(page.getByText('Hotel updated successfully')).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name=name]')).toHaveValue('Pink Three');
+  await page.locator('[name=name]').fill('Pink Three(Hong Ba)');
+  await page.getByRole('button', {name: 'Save'}).click();
+  page.setDefaultTimeout(10000);
+  await expect(page.getByText('Hotel updated successfully')).toBeVisible();
+});
