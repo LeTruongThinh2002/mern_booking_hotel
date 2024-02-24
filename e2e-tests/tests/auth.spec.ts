5fd37ab751804262a832498fe04e71faade0c770
thinhofdakh <0306201587@caothang.edu.vn>
2024-02-24 10:12:31 +0700

    finish_test_search_hotels

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,19 @@
 import {test, expect} from '@playwright/test';
 
 const UI_URL = 'http://localhost:5173/';
-
 test('should allow user to register', async ({page}) => {
   await page.goto(UI_URL);
   //get the sign in button
   await page.getByRole('link', {name: 'Sign In'}).click();
   await page.getByRole('link', {name: 'Create an account here'}).click();
 
-  await expect(page.getByRole('heading', {name: 'Create an account'})).toBeVisible();
+  await expect(
+    page.getByRole('heading', {name: 'Create an account'})
+  ).toBeVisible();
 
   await page.locator('[name=firstName]').fill('thinh');
   await page.locator('[name=lastName]').fill('le');
-  await page.locator('[name=email]').fill('thinhofdakh@gmail.com');
+  await page.locator('[name=email]').fill('LTruongThinh2002@gmail.com');
   await page.locator('[name=password]').fill('eBpd.tnAMK8X*Z');
   await page.locator('[name=confirmPassword]').fill('eBpd.tnAMK8X*Z');
 
@@ -31,8 +32,8 @@ test('should not allow the user to sign in', async ({page}) => {
 
   await expect(page.getByRole('heading', {name: 'Sign In'})).toBeVisible();
 
-  await page.locator('[name=email]').fill('thinhofdakh@gmail.com');
-  await page.locator('[name=password]').fill('eBpd.tnAMK8X*Z');
+  await page.locator('[name=email]').fill('LTruongThinh2002@gmail.com');
+  await page.locator('[name=password]').fill('Thinhzs2a@');
 
   await page.getByRole('button', {name: 'Log In'}).click();
 
---
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -0,0 +1,30 @@
+import {test, expect} from '@playwright/test';
+
+const UI_URL = 'http://localhost:5173/';
+
+test('should not allow the user to sign in', async ({page}) => {
+  await page.goto(UI_URL);
+  //get the sign in button
+  await page.getByRole('link', {name: 'Sign In'}).click();
+
+  await expect(page.getByRole('heading', {name: 'Sign In'})).toBeVisible();
+
+  await page.locator('[name=email]').fill('LTruongThinh2002@gmail.com');
+  await page.locator('[name=password]').fill('Thinhzs2a@');
+
+  await page.getByRole('button', {name: 'Log In'}).click();
+
+  await expect(page.getByText('Sign In Successful!')).toBeVisible();
+  await expect(page.getByRole('link', {name: 'My Bookings'})).toBeVisible();
+  await expect(page.getByRole('link', {name: 'My Hotels'})).toBeVisible();
+  await expect(page.getByRole('button', {name: 'Sign Out'})).toBeVisible();
+});
+
+test('should show hotels search results', async ({page}) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder('Where are you going').fill('Hồ Chí Minh');
+  await page.getByRole('button', {name: 'Search'}).click();
+
+  await expect(page.getByText('Hotels founded in Hồ Chí Minh')).toBeVisible();
+});
