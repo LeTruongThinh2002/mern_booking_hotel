7953b989b834591a0ec7917d62bb23e3abfe6231
thinhofdakh <0306201587@caothang.edu.vn>
2024-02-29 15:43:21 +0700

    last

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,12 @@ export default {
         }
       }
     },
-    container: {}
+    container: {
+      padding: [
+        'sm'='auto',
+        'md'= '10rem'
+      ]
+    }
   },
   plugins: []
 };
